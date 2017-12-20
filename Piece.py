62996e450cbe9afe5a256850f3a034030ef9f856
Justin Knueppel <justinknueppel@gmail.com>
2017-12-19 20:26:56 -0500

    Create getters and setters for Piece

---
+++ Piece.py
@@ -0,0 +1,26 @@
+class Piece:
+    def __init__(self, team, position):
+        self.team = team
+        self.position = position
+        self.king = False
+        self.pos_moves = set()
+
+    def move (self, newpos):
+        if newpos in self.pos_moves:
+            self.position - newpos
+        else:
+            return False
+
+    def get_pos_moves(self):
+        return self.pos_moves
+
+    def set_pos_moves(self, moves):
+        self.pos_moves = set()
+        for move in moves:
+            self.pos_moves.add(move)
+
+    def get_team (self):
+        return self.team
+
+    def get_king(self):
+        return self.king
