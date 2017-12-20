8ccc9c60b6846c9adf6bca1c57b321161215e93c
Justin Knueppel <justinknueppel@gmail.com>
2017-12-20 13:32:36 -0500

    add an inverted display for the second player

--- Board.py
+++ Board.py
@@ -8,16 +8,16 @@ class Board:
                 #creates [letter][number] ID system
                 key = '{0}{1}'.format(letter, num)
                 #0 = unoccupied square, 1 = occupied by team 1, 2 = occupied by team 2, - = unoccupiable
-                val = 0
+                val = '0'
                 if (letter in 'aceg' and num % 2 == 0) or (letter in 'bdfh' and num % 2 == 1):
                     val = '-'
                 elif (num in [1,3] and letters.index(letter) % 2 == 0) or (num == 2 and letters.index(letter)  == 1):
-                    val = 1
+                    val = '1'
                 elif (num in [6,8] and letters.index(letter) % 2 == 1) or (num == 7 and letters.index(letter)  % 2 == 0):
-                    val = 2
+                    val = '2'
                 self.board[key] = val
 
-    def display(self):
+    def display1(self):
         print('  ___________________')
         print(' |                   |')
         #print nums backwards to build board top down
@@ -30,5 +30,19 @@ class Board:
         print(' |___________________|')
         print('    a b c d e f g h')
 
+    def display2(self):
+        print('  ___________________')
+        print(' |                   |')
+        #print nums backwards to build board top down
+        for num in '12345678':
+            print(num, end = '|  ')
+            for letter in 'hgfedcba':
+                key = '{0}{1}'.format(letter, num)
+                print(self.board[key], end = ' ')
+            print(' |')
+        print(' |___________________|')
+        print('    h g f e d c b a')
+
 board = Board()
-board.display()
+board.display1()
+board.display2()
