1de91e5893cab765f299f960f7101bcf4c29576e
Hoopsters35 <32350916+Hoopsters35@users.noreply.github.com>
2017-12-21 21:38:57 -0500

    get rid of getters on piece class

--- Piece.py
+++ Piece.py
@@ -12,19 +12,10 @@ class Piece:
         else:
             return False
 
-    def get_pos_moves(self):
-        return self.pos_moves
-
     def set_pos_moves(self, moves):
         self.pos_moves = set()
         for move in moves:
             self.pos_moves.add(move)
-
-    def get_team (self):
-        return self.team
-
-    def get_king(self):
-        return self.king
 
     def king(self):
         self.king = True
