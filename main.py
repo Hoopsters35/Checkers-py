c13497c11fa12b75ef5401511f51ce77b0c2b2bd
Justin Knueppel <justinknueppel@gmail.com>
2017-12-22 10:07:43 -0500

    use regular expressions in main to capture user commands

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
-import board
+import board, re
+
 board = board.Board()
 t1 = True
+
+moveregex = re.compile('^([a-h][1-8]) ([a-h][1-8])$')
+
 while board.hasmove(t1):
     if t1:
         board.display1()
         print('Team 1 to move.')
         inp = input('Enter move [ie. a3 b4] ')
-        while not (len(inp) == 5 and inp[:2] in board.squares1 and inp[2] == ' ' and inp[3:5] in board.possiblemoves(inp[:2])):
+        while not moveregex.match(inp):
             print('Error with move, please use format:a3 b4')
             inp = input('Enter move: ')
-        board.move(inp[:2], inp[3:5])
+        board.move(moveregex.group(1), moveregex.group(2))
         t1 = !t1
     else:
         board.display2()
         print('Team 2 to move.')
         inp = input('Enter move [ie. a3 b4] ')
-        while not (len(inp) == 5 and inp[:2] in board.squares2 and inp[2] == ' ' and inp[3:5] in board.possiblemoves(inp[:2])):
+        while not moveregex.match(inp):
             print('Error with move, please use format:a3 b4')
             inp = input('Enter move: ')
-        board.move(inp[:2], inp[3:5])
+        board.move(moveregex.group(1), moveregex.group(2))
         t1 = !t1
 #TODO display winner
 
