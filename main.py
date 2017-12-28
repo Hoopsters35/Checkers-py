ef177dcc51f30f74cd645fd15fe5798a526226e6
Justin Knueppel <justinknueppel@gmail.com>
2017-12-28 13:10:10 -0500

    first attempt at multicapture in main -- currently does not work

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import board, re
 
-board = board.Board()
-t1 = True
-
 def getvalidmove(team):
     moveregex = re.compile('^([a-h][1-8]) ([a-h][1-8])$')
     inp = input('Enter move [ie. a3 b4] ')
@@ -19,6 +16,9 @@ def getvalidmove(team):
         cmd = moveregex.match(inp)
     return cmd
 
+board = board.Board()
+t1 = True
+
 while board.hasmove(t1):
     board.display(t1)
     if t1:
@@ -27,6 +27,16 @@ while board.hasmove(t1):
         print('Team 2 to move.')
     cmd = getvalidmove(t1)
     board.move(cmd.group(1), cmd.group(2))
+    while board.possiblemoves(cmd.group(1))[cmd.group(2)] == True:
+        ask = input('Make follow-up capture? [Y/N]: ')
+        if ask.lower() == 'y':
+            cmd2 = getvalidmove(t1)
+            while not (cmd2.group(1) == cmd.group(2) and board.possiblemoves(cmd2.group(2))[cmd2.group(2)] == True):
+                print('You must use', cmd.group(2), 'to make a capturing move.')
+                cmd2 = getvalidmove(t1)
+            board.move(cmd2.group(1), cmd2.group(2))
+            cmd = cmd2
+        else: break
     t1 = not t1
 #TODO display winner
 if t1 == True:
