4f4795c73ae4e6deade469226e10e2e08e8c9eb1
Justin Knueppel <justinknueppel@gmail.com>
2017-12-28 15:29:23 -0500

    comment through multicapture

--- main.py
+++ main.py
@@ -32,24 +32,32 @@ while board.hasmove(t1):
         #add if statement for if another capture is possible, otherwise break
         anothercap = False
         posmoves = board.possiblemoves(cmd.group(2))
+        #iterate through possible moves for the new square to see if there are any new capturing moves
         for i in posmoves.keys():
             if posmoves[i] == True:
                 anothercap = True
+        #if there is not, the turn ends
         if not anothercap: break
         else:
+            #display board after first capture
             board.display(t1)
             ask = input('Make follow-up capture? [Y/N]: ')
             if ask.lower() == 'y':
                 cmd2 = getvalidmove(t1)
+                #after getting a traditional valid move, further restrict that move
+                #must use same piece and capture again
                 while not (cmd2.group(1) == cmd.group(2) and board.possiblemoves(cmd2.group(1))[cmd2.group(2)] == True):
                     print('You must use', cmd.group(2), 'to make a capturing move.')
                     cmd2 = getvalidmove(t1)
+                #not sure if this redefinition of capture is necessary as the move must be a cap
                 capture = board.possiblemoves(cmd2.group(1))[cmd2.group(2)]
                 board.move(cmd2.group(1), cmd2.group(2))
+                #makes the new move the old move in order to check for a next jump
                 cmd = cmd2
+            #end turn if they do not want additional capture
             else: break
     t1 = not t1
-#TODO display winner
+#display winner
 if t1 == True:
     print('Congratulations team 2! You win!')
 else:
