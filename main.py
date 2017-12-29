4dfc2cfde3f752610288896eead581483188a574
Justin Knueppel <justinknueppel@gmail.com>
2017-12-28 20:00:09 -0500

    start to add more descriptive variable and method names

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import board, re
 
-def getvalidmove(team):
+def get_valid_move(team):
     moveregex = re.compile('^([a-h][1-8]) ([a-h][1-8])$')
-    inp = input('Enter move [ie. a3 b4] ')
-    cmd = moveregex.match(inp)
-    while not (cmd and cmd.group(1) in board.squares[t1] and cmd.group(2) in board.possiblemoves(cmd.group(1)).keys()):
+    proposedMove = input('Enter move [ie. a3 b4] ')
+    confirmedMove = moveregex.match(proposedMove)
+    while not (confirmedMove and confirmedMove.group(1) in board.squares[t1] and confirmedMove.group(2) in board.possiblemoves(confirmedMove.group(1)).keys()):
         print('Error with move, please use format:a3 b4')
-        if not cmd:
+        if not confirmedMove:
             print('Does not match format')
-        elif cmd.group(1) not in board.squares[t1]:
-            print(cmd.group(1), 'not in your camp')
-        elif cmd.group(2) not in board.possiblemoves(cmd.group(1)).keys():
-            print(cmd.group(2), 'is not a possible square for', cmd.group(1))
-        inp = input('Enter move: ')
-        cmd = moveregex.match(inp)
-    return cmd
+        elif confirmedMove.group(1) not in board.squares[t1]:
+            print(confirmedMove.group(1), 'not in your camp')
+        elif confirmedMove.group(2) not in board.possiblemoves(confirmedMove.group(1)).keys():
+            print(confirmedMove.group(2), 'is not a possible square for', confirmedMove.group(1))
+        proposedMove = input('Enter move: ')
+        confirmedMove = moveregex.match(proposedMove)
+    return confirmedMove
 
 board = board.Board()
 t1 = True
@@ -25,7 +25,7 @@ while board.hasmove(t1):
         print('Team 1 to move.')
     else:
         print('Team 2 to move.')
-    cmd = getvalidmove(t1)
+    cmd = get_valid_move(t1)
     capture = board.possiblemoves(cmd.group(1))[cmd.group(2)]
     board.move(cmd.group(1), cmd.group(2))
     while capture == True:
@@ -43,12 +43,12 @@ while board.hasmove(t1):
             board.display(t1)
             ask = input('Make follow-up capture? [Y/N]: ')
             if ask.lower() == 'y':
-                cmd2 = getvalidmove(t1)
+                cmd2 = get_valid_move(t1)
                 #after getting a traditional valid move, further restrict that move
                 #must use same piece and capture again
                 while not (cmd2.group(1) == cmd.group(2) and board.possiblemoves(cmd2.group(1))[cmd2.group(2)] == True):
                     print('You must use', cmd.group(2), 'to make a capturing move.')
-                    cmd2 = getvalidmove(t1)
+                    cmd2 = get_valid_move(t1)
                 #not sure if this redefinition of capture is necessary as the move must be a cap
                 capture = board.possiblemoves(cmd2.group(1))[cmd2.group(2)]
                 board.move(cmd2.group(1), cmd2.group(2))
